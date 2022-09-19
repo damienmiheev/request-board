5eea61852215e4e1287167d2381d3e8af40477f4
ekaterynatarasenko <damienmiheev@gmail.com>
2022-09-19 17:17:51 +0300

    Initialisation

---
+++ src/main/java/com/example/requestboard/RequestBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.requestboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/requestboard/RequestBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.requestboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
